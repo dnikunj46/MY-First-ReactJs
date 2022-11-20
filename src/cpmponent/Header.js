5b939ab83bc4f7fbf701c068855050633425aa59
DESAI NIKUNJ <dnikunj46@gmail.com>
2022-11-20 14:04:03 +0530

    first commit

---
+++ src/cpmponent/Header.js
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <>
+        <h1>This Header Belongs to Function Header.</h1>
+        </>
+    );
+}
+
+export default Header;
---
+++ src/cpmponent/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Sidebar extends React.Component
+{
+    render() {
+        return (
+<>
+            <h2>This Element is from class based component.</h2>
+            </>
+        );
+    }
+}
+export default Sidebar;
